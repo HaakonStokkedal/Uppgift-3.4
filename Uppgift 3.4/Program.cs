cd2351f1c7a1aaebd2eb484d37f67c94996376a1
hast06001 <hast06001@E-PW024NW1>
2023-10-06 11:10:21 +0200

    Add project files.

---
+++ Uppgift 3.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uppgift_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många minuter är din låt? (ex. 3 min och 40 sekunder blir 3 min)");
+            int minuter = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många sekunder är den? (ex. 3 min och 40 sekunder blir 40 sekunder)");
+            int sekunder = int.Parse(Console.ReadLine());
+            int sekunderTotal = (minuter * 60) + sekunder;
+
+            if (sekunderTotal>=165 && sekunderTotal<=260)
+                Console.WriteLine("Din låt får spelas på radiostationen.");
+            else
+                Console.WriteLine("Din låt får inte spelas på radiostationen.");
+
+            Console.ReadKey();
+        }
+    }
+}
